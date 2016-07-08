f1580b7d4cc4aa03542993de42c94ab0702dec1e
modularjon <modularjon@gmail.com>
2016-07-08 15:29:50 -0400

    Refactor module for awsupload

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,39 +1,8 @@
 'use strict';
 
-require('dotenv').config();
-
 const fs = require('fs');
-const crypto = require('crypto');
 
-const fileType = require('file-type');
-const AWS = require('aws-sdk');
-const awsModule = require('aws-s3-upload')
-
-const s3 = new AWS.S3({
-  credentials: {
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  }
-});
-
-const mimeType = (data) => {
-  return Object.assign({
-    ext: 'bin',
-    mime: 'application/octet-stream',
-  }, fileType(data));
-};
-
-const randomHexString = (length) => {
-  return new Promise((resolve, reject) => {
-    crypto.randomBytes(length, (error, buffer) => {
-      if (error) {
-        reject(error);
-      }
-
-      resolve(buffer.toString('hex'));
-    });
-  });
-};
+const uploader = require('../lib/aws-s3-upload');
 
 let filename = process.argv[2] || '';
 //reminder: this is how you get stuff FROM the command line
@@ -50,38 +19,8 @@ const readFile = (filename) => {
   });
 };
 
-const awsUpload = (file) => {
-  return randomHexString(16)
-  .then((filename) => {
-    let dir = new Date().toISOString().split('T')[0];
-
-    return {
-      ACL: 'public-read',
-      Body: file.data,
-      Bucket: 'modularjon.bucket',
-      ContentType: file.mime,
-      Key: `${dir}/${filename}.${file.ext}`,
-    };
-  })
-  .then((options) => {
-    return new Promise((resolve, reject) => {
-      s3.upload(options, (error, data) => {
-        if (error) {
-          reject(error);
-        }
-
-        resolve(data);
-      });
-    });
-  });
-};
-
 readFile(filename)
-.then((data) => {
-  let file = mimeType(data);
-  file.data = data;
-  return file;
-})
-.then(awsUpload)
+.then(uploader.prepareFile)
+.then(uploader.awsUpload)
 .then(console.log) //(data) => console.log(`${filename} is ${data.length} bytes long`))
 .catch(console.error);
